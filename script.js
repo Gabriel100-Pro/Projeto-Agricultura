3a6d741cbbb629e5feabdc20f02be391ff5c240c
Gabriel100-Pro <cortesempreender2.0@gmail.com>
2026-07-01 19:07:12 -0300

    inicio da criação do projeto da Raiz Silvestre

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuToggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
+
+if (menuToggle && menu) {
+  menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+
+  menu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("open");
+    });
+  });
+}
+
+AOS.init({
+  duration: 900,
+  easing: "ease-out-cubic",
+  once: true,
+  offset: 80
+});
